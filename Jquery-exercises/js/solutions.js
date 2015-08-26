9df9054853b1865466dc93a101506d4b26b4a153
FacundoPomar <facundopomar@hotmail.com>
2015-08-26 15:01:34 -0300

    Finished all remaining Activities

--- Jquery-exercises/js/solutions.js
+++ Jquery-exercises/js/solutions.js
@@ -40,34 +40,60 @@ $(".input_text[name='q']").parent().addClass("a-new-class");
 // Remove that class from it
 // Add a class of "current" to the next list item
 
-
+$("#myList .current").removeClass("current").next().addClass("current")
 
 // Select the select element inside #specials
 // Traverse your way to the submit button.
 
-
+$("#specials select").parent().next().children().first()
 
 // Select the first list item in the #slideshow element
 // Add the class "current" to it, and then add a class of "disabled" to its sibling elements
 
-
+$("#slideshow").children().first().addClass("current").siblings().each(function () {
+    $(this).addClass("disabled");
+});
 
 // Add five new list items to the end of the unordered list #myList
 
-
+var amount = $("#myList").children().length;
+for (var i = amount +1; i < amount + 6; i++) {
+    $("#myList").append($("<li></li>", {text: "List item " + i}));
+}
 
 // Remove the odd list items
 
-
+$("#myList li:odd").remove();
 
 // Add another h2 and another paragraph to the last div.module
 
-
+$(".module")
+.last()
+.append($("<h2></h2>", {
+    text : "I'm a the new H2 title"
+}))
+.append($("<p></p>", {
+    text : "I'm a the new paragraph"
+}))
 
 // Add another option to the select element
 // Give the option the value "Wednesday“
 
-
+$("#specials form ul li select")
+.append($("<option></option>", {
+    text: "Wednesday",
+    value : "Wednesday"
+}));
 
 // Add a new div.module to the page after the last one
 // Put a copy of one of the existing images inside of it
+
+//Una variación a usar .last()
+$(".module:last")
+.after($("<div></div>", {
+    class: "module"
+})
+.append($("img")
+    .last()
+    .clone())
+)
