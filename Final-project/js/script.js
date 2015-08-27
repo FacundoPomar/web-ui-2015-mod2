eb615c188897965d4429676ab841ed149ef1fab5
FacundoPomar <facundopomar@hotmail.com>
2015-08-27 13:35:22 -0300

    bugfix visual efects on changing selected vehicle

--- Final-project/js/script.js
+++ Final-project/js/script.js
@@ -1,32 +1,57 @@
 Number.prototype.toFixedDown = function(digits) {
     var re = new RegExp("(\\d+\\.\\d{" + digits + "})(\\d)"),
-        m = this.toString().match(re);
+    m = this.toString().match(re);
     return m ? parseFloat(m[1]) : this.valueOf();
 };
 
-var currentVehicle = {};
-var currentVehicleUI = {};
+var currentVehicle = undefined;
+var currentVehicleUI = undefined;
+
+var vehicles = {
+    "land" : {
+        "constructor" : LandVehicle,
+        "UI" : LandVehicleUI,
+    },
+    "air" : {
+        "constructor" : AirVehicle,
+        "UI" : AirVehicleUI,
+    }
+}
 
 $(document).ready(function () {
 
-    var vehicles = {
-        "land" : {
-            "constructor" : LandVehicle,
-            "UI" : LandVehicleUI,
-        }
-    }
+
 
     $(".vehicle-option").slideDown();
 
     $(".vehicle-option").on("click", function () {
-        //Select current ui-class
-        currentVehicle = vehicles[$(this).attr("vehicle-type")]["constructor"]();
-        currentVehicleUI = vehicles[$(this).attr("vehicle-type")]["UI"](currentVehicle);
-        currentVehicleUI.renderControls(".vehicle-fields");
-        currentVehicleUI.renderAnimation(".vehicle-animation");
-        $(".vehicle-fields").slideDown();
-        $(".vehicle-animation").slideDown();
+
+        var elem = this;
+
+        $(".vehicle-fields").slideUp();
+        $(".vehicle-animation").slideUp(function () {
+            initialiceVehicle(elem, function () {
+               $(".vehicle-fields").slideDown();
+               $(".vehicle-animation").slideDown();
+           })
+        });
+
+
+
+
     });
 
 });
+
+function initialiceVehicle(option, callback) {
+
+    currentVehicle = vehicles[$(option).attr("vehicle-type")]["constructor"]();
+    currentVehicleUI = vehicles[$(option).attr("vehicle-type")]["UI"](currentVehicle);
+    currentVehicleUI.renderControls(".vehicle-fields");
+    currentVehicleUI.renderAnimation(".vehicle-animation");
+
+    if (callback) {
+        callback();
+    }
+}
 
